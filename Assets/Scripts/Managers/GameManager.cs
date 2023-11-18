9680f20c8bb45a7871961d50c11437e35308c5f4
mushuacan <mushuacan@gmail.com>
2023-11-18 14:08:16 +0300

    Revert "Revert "Game Manager Created""
    This reverts commit 8957147e412205c5945f4cb95d6f23735a9004b8.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private GameObject MainGameObject;
+
+    #endregion
+
+    #region Action Variables
+
+
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+
+    }
+
+    #endregion
+
+    #region Custom Methods
+
+
+
+    #endregion
+}
