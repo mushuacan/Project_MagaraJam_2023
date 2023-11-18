a672824e65db34c60e28f47f25644de36d7bde39
mushuacan <mushuacan@gmail.com>
2023-11-18 15:53:51 +0300

    LazerMove ayarlamaları

--- Assets/Scripts/LazerMove.cs
+++ Assets/Scripts/LazerMove.cs
@@ -5,15 +5,18 @@ using DG.Tweening;
 
 public class LazerMove : MonoBehaviour
 {
+    private int derece;
+    private bool lazerActive;
+    private Tween moveTween;
+    [SerializeField] private Ease moveEase;
+    private AllColors lazerColor;
+    private int lazerTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    private int derece;
-    private bool lazerActive;
-    private Tween moveTween;
-    [SerializeField] private Ease moveEase;
 
     private void OnEnable()
     {
@@ -26,13 +29,52 @@ public class LazerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             lazerActive = false;
-            transform.position = Vector2.zero;
-            Debug.Log("Space detected");
-            derece = Random.Range(0, 360);
-            transform.Rotate(0, 0, derece);
-            transform.position += transform.right * 12;
+            ColorChoose(Random.Range(1, 9));//~%22 ana renkler ~%11 ara renkler || Ayrýca seçtiði renge göre hýzýný da ayarlar
+            Konumlan();
             lazerActive = true;
-            moveTween = transform.DOMove(new Vector3(0, 0, 0), 4, false).SetEase(moveEase);
+            moveTween = transform.DOMove(new Vector3(0, 0, 0), lazerTimer, false).SetEase(moveEase); //Harekete geç
+        }
+    }
+    void Konumlan()
+    {
+        transform.position = Vector2.zero;
+        derece = Random.Range(0, 360);
+        transform.Rotate(0, 0, derece);
+        transform.position += transform.right * 12;
+    }
+    void ColorChoose(int Chooser)
+    {
+        if (Chooser == 1 || Chooser == 2) //ana renkler
+        {
+            lazerColor = AllColors.Red;
+        }
+        else if (Chooser == 3 || Chooser == 4)
+        {
+            lazerColor = AllColors.Blue;
+        }
+        else if (Chooser == 5 || Chooser == 6)
+        {
+            lazerColor = AllColors.Green;
+        }
+        else if (Chooser == 7) //ara renkler
+        {
+            lazerColor = AllColors.Cyan;
+        }
+        else if (Chooser == 8)
+        {
+            lazerColor = AllColors.Magenta;
+        }
+        else if (Chooser == 9)
+        {
+            lazerColor = AllColors.Yellow;
+        }
+        if (Chooser < 7) //Hýz ayarý
+        {
+            lazerTimer = 4;
+        }
+        else
+        {
+            lazerTimer = 6;
         }
     }
 }
