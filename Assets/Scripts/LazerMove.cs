d0f96575dddbaba68ddb69feff211814e42e123e
mushuacan <mushuacan@gmail.com>
2023-11-19 12:39:55 +0300

    Lazer Rain Arranged

--- Assets/Scripts/LazerMove.cs
+++ Assets/Scripts/LazerMove.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEditor.Tilemaps;
+using Unity.Mathematics;
 
 public class LazerMove : MonoBehaviour
 {
     private int derece;
+    public static int choosenLazerColor;
     private bool lazerActive;
     private bool hasCreated = false;
     private Tween moveTween;
     [SerializeField] private Ease moveEase;
-    public AllColors lazerColor;  
+    public AllColors lazerColor;
     private int lazerTimer;
     private SpriteRenderer spriteRenderer;
 
@@ -24,7 +26,7 @@ public class LazerMove : MonoBehaviour
     {
         if (hasCreated)
         {
-            ColorChoose(Random.Range(1, 10));
+            ColorChoose(SpawnManager.Instance.lazerType);
             DOVirtual.DelayedCall(0.1f, SetObjectColor);
             TakePlace();
             moveTween = transform.DOMove(new Vector3(0, 0, 0), lazerTimer, false).SetEase(moveEase); //Harekete geç
@@ -64,7 +66,7 @@ public class LazerMove : MonoBehaviour
     void TakePlace()
     {
         transform.position = Vector2.zero;
-        derece = Random.Range(0, 360);
+        derece = UnityEngine.Random.Range(0, 360);
         transform.Rotate(0, 0, derece);
         transform.position += transform.right * 12;
     }
@@ -73,14 +75,37 @@ public class LazerMove : MonoBehaviour
     {
         if (ColorManager.Instance != null)
         {
-            
             spriteRenderer.color = ColorManager.Instance.ReturnColor(lazerColor);
-
         }
     }
 
-    void ColorChoose(int Chooser)
+    void ColorChoose(string Mode)
     {
+        int Chooser;
+        if (Mode == "Ana")
+        {
+            Chooser = UnityEngine.Random.Range(1, 7);
+        }
+        else if (Mode == "Ara")
+        {
+            Chooser = UnityEngine.Random.Range(7, 10);
+        }
+        else if (Mode == "All")
+        {
+            Chooser = UnityEngine.Random.Range(1, 10);
+        }
+        else if (Mode == "2Ana")
+        {
+            do
+            {
+                Chooser = UnityEngine.Random.Range(1, 7);
+            } while (Mathf.Abs(Chooser - choosenLazerColor) <= 1);
+            choosenLazerColor = Chooser;
+        }
+        else
+        {
+            Chooser = UnityEngine.Random.Range(1, 7);
+        }
         //~%22 ana renkler ~%11 ara renkler || Ayrýca seçtiði renge göre hýzýný da ayarlar
         if (Chooser == 1 || Chooser == 2) //ana renkler
         {
@@ -106,13 +131,17 @@ public class LazerMove : MonoBehaviour
         {
             lazerColor = AllColors.Yellow;
         }
-        if (Chooser < 7) //Hýz ayarý
+        if (Time.time < 100) //Hýz ayarý
         {
-            lazerTimer = 4;
+            lazerTimer = 6; 
         }
-        else
+        else if (Chooser < 7)
         {
-            lazerTimer = 6;
+            lazerTimer = 6; //ana renkler
+        }
+        else if (Chooser < 10)
+        {
+            lazerTimer = 7; //ara renkler
         }
     }
 }
--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -7,8 +7,17 @@ using Unity.VisualScripting;
 
 public class SpawnManager : MonoBehaviour
 {
-
+    public static SpawnManager Instance { get; private set; }
     private float laserSpawnTime;
+    public string lazerType;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
 
     private void OnEnable()
     {
@@ -26,15 +35,74 @@ public class SpawnManager : MonoBehaviour
         StartCoroutine(LazerCreate());
     }
 
-    IEnumerator LazerCreate()
+    private void LazerSetActive()
     {
         GameObject lazer = LazerPool.SharedInstance.GetPooledObject();
         if (lazer != null)
         {
             lazer.SetActive(true);
         }
-        yield return new WaitForSeconds(laserSpawnTime);
+    }
 
+    IEnumerator LazerCreate()
+    {
+        
+        Debug.Log(Time.time);
+        
+        if (Time.time < 10)
+        {
+            lazerType = "Ana";
+            laserSpawnTime = 4;
+            LazerSetActive();
+
+        }
+        else if (Time.time < 60)
+        {
+            lazerType = "Ana";
+            laserSpawnTime = 3;
+            LazerSetActive();
+        }
+        else if (Time.time < 90)
+        {
+            lazerType = "2Ana";
+            laserSpawnTime = 4;
+            LazerSetActive();
+            LazerSetActive();
+        }
+        else if (Time.time < 100)
+        {
+            lazerType = "Ara";
+            laserSpawnTime = 5;
+            LazerSetActive();
+        }
+        else
+        {
+            int lazerRandom = Random.Range(0, 100);
+            if (lazerRandom <= 50)
+            {
+                lazerType = "2Ana";
+                laserSpawnTime = 3;
+                LazerSetActive();
+                LazerSetActive();
+            } else if (lazerRandom <= 70)
+            {
+                lazerType = "Ara";
+                laserSpawnTime = 3;
+                LazerSetActive();
+            } else if (lazerRandom <= 90)
+            {
+                lazerType = "Ana";
+                laserSpawnTime = 2;
+                LazerSetActive();
+            } else if(lazerRandom <= 100)
+            {
+                lazerType = "All";
+                laserSpawnTime = 2;
+                LazerSetActive();
+            }
+        }
+        
+        yield return new WaitForSeconds(laserSpawnTime);
         if (GameManager.Instance.IsGameOn)
         {
             StartCoroutine(LazerCreate());
