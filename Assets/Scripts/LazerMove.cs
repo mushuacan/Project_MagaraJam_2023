26cb28217d34201bfddd55fdeba735298b64e879
YusaCan <ahmedyusacan@gmail.com>
2023-11-19 18:02:19 +0300

    updates

--- Assets/Scripts/LazerMove.cs
+++ Assets/Scripts/LazerMove.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.Tilemaps;
 using Unity.Mathematics;
 
 public class LazerMove : MonoBehaviour
